beafb3973fd95fd9af12051c5d7fb14bcd0fe8f4
Dave Haynes <dave.haynes@downcastsystems.com>
2014-07-21 13:44:35 -0400

    some changes in main.lua

--- main.lua
+++ main.lua
@@ -38,32 +38,3 @@ local storyboard = require "storyboard"
 storyboard.gotoScene( "scripts.scenes.scene_results" )
 
 
-
--- Displaying fonts on system
---local fonts = native.getFontNames()
---
---count = 0
---
----- Count the number of total fonts
---for i,fontname in ipairs(fonts) do
---    count = count+1
---end
---
---print( "\rFont count = " .. count )
---
---local name = "georgia"     -- part of the Font name we are looking for
---
---name = string.lower( name )
---
----- Display each font in the terminal console
---for i, fontname in ipairs(fonts) do
---    j, k = string.find( string.lower( fontname ), name )
---
---    if( j ~= nil ) then
---
---        print( "fontname = " .. tostring( fontname ) )
---
---    end
---end
-
-
